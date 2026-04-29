4b16227e5c3c254e3c94df2cde4244b065ed240b
Priyankkhatri <priyankkhatri1980@gmail.com>
2026-04-29 11:10:31 +0530

    feat: add replace note endpoint (PUT /api/notes/:id)

--- src/controllers/note.controller.js
+++ src/controllers/note.controller.js
@@ -162,6 +162,42 @@ const getNoteById = async (req, res) => {
   }
 };
 
+const replaceNote = async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    if (!isValidObjectId(id)) {
+      return res.status(400).json({
+        success: false,
+        message: "Invalid note ID",
+        data: null,
+      });
+    }
+
+    const note = await Note.findByIdAndUpdate(id, req.body, {
+      new: true,
+      overwrite: true,
+      runValidators: true,
+    });
+
+    if (!note) {
+      return res.status(404).json({
+        success: false,
+        message: "Note not found",
+        data: null,
+      });
+    }
+
+    return res.status(200).json({
+      success: true,
+      message: "Note replaced successfully",
+      data: note,
+    });
+  } catch (error) {
+    return sendServerError(res, error);
+  }
+};
+
 module.exports = {
   allowedSortFields,
   sendServerError,
@@ -174,4 +210,5 @@ module.exports = {
   createBulkNotes,
   getAllNotes,
   getNoteById,
+  replaceNote,
 };
